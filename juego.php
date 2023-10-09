c46e63ae626e02268728d40634843b19a0a91eb6
Javier5464 <131973021+Javier5464@users.noreply.github.com>
2023-10-09 19:57:27 +0200

    Create juego.php

---
+++ juego.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piedra, Papel, Tijera</title>
+</head>
+<body>
+    <h1>PIEDRA, PAPEL, TIJERA!</h1>
+
+    <?php
+        $tabla = [
+            [0, 2, 1],
+            [1, 0, 2],
+            [2, 1, 0]
+        ];
+
+        $msg = [
+            "Empate",
+            "Ha ganado el jugador 1",
+            "Ha ganado el jugador 2"
+        ];
+
+        $opciones = [
+            "piedra" => "&#x1F91C;",
+            "papel" => "&#x1F91A;",
+            "tijera" => "&#x1F596;"
+        ];
+
+        $a = [
+            "piedra" => 0,
+            "papel" => 1,
+            "tijera" => 2,
+        ];
+
+        $jugador1 = array_rand($opciones);
+        $jugador2 = array_rand($opciones);
+
+        function ganador($jugador1, $jugador2, $tabla, $msg, $a) {
+            $resultado = $tabla[$a[$jugador1]][$a[$jugador2]];
+            return $msg[$resultado];
+        }
+    ?>
+    <h3>Jugador 1: </h3>  <?php echo $opciones[$jugador1] ?> <br>
+    <h3>Jugador 2: </h3><?php echo $opciones[$jugador2]  ?> <br>
+    <h3>Resultado: </h3>  <?php echo ganador($jugador1, $jugador2, $tabla, $msg, $a)?>
+
+</body>
+</html>
